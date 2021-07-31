9e939509b56fa692f9065b410434d03f5d361988
HameedMulani <hameedmulani2001@gmail.com>
2021-07-31 10:02:30 +0530

    initial commit

---
+++ output1.php
@@ -0,0 +1,52 @@
+<?php 
+// echo "Entry"; exit;
+	$response = [];
+	$response['status'] = "Failure";
+	$response['data'] = $_FILES['video_file'];
+
+	if ($_FILES["audio_file"]["error"] > 0)
+	$response['message']['audio'] = "Return Code: " . $_FILES["audio_file"]["error"];
+	else
+	{
+	  // move_uploaded_file($_FILES["audio_file"]["tmp_name"],
+	  // "videos/" . $_FILES["audio_file"]["name"]);
+
+	  // echo "Audio"; exit;
+
+	  $response['message']['audio'] = "Stored in: ";
+	}
+
+	if ($_FILES["video_file"]["error"] > 0)
+	$response['message']['video'] = "Return Code: " . $_FILES["video_file"]["error"];
+	else
+	{
+	if(file_exists('videos/video_file.webm'))
+	{
+		unlink('upload/video_file.webm');
+		move_uploaded_file($_FILES["video_file"]["tmp_name"],
+	  "videos/" . $_FILES["video_file"]["name"]);
+
+	  $response['message']['video'] = "Stored in: ";
+	}
+	else{
+	  move_uploaded_file($_FILES["video_file"]["tmp_name"],
+	  "videos/" . $_FILES["video_file"]["name"]);
+
+	  $response['message']['video'] = "Stored in: ";
+	}
+	}
+	
+		 
+	// if(shell_exec('ffmpeg -i upload/audio.mp4' . ' -i upload/video.mp4 -c copy upload/audio_filevideo_file.webm'))
+	// {
+	// 	$response['message']['upload'] = "uploaded in: ";
+	// }
+
+ //    unlink('upload/' . $_FILES["audio_file"]["name"]);
+	// unlink('upload/' . $_FILES["video_file"]["name"]);
+
+	$response['status'] = "Success";
+
+	echo json_encode($response);
+
+?>
